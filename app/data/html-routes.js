aebe5385fa8659096f15f1dfb741783c10c9ec91
Fiona Ballard <fionaballard@Fiona-2.local>
2016-12-17 15:20:22 -0500

    ho

---
+++ app/data/html-routes.js
@@ -0,0 +1,30 @@
+// ===============================================================================
+// DEPENDENCIES
+// We need to include the path package to get the correct file path for our html
+// ===============================================================================
+var path = require('path');
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function (app) {
+	// HTML GET Requests
+	// Below code handles when users "visit" a page.
+	// In each of the below cases the user is shown an HTML page of content
+	// ---------------------------------------------------------------------------
+
+	app.get('/home', function (request, response) {
+		response.sendFile(path.join(__dirname + '/../public/home.html'));
+	});
+
+	app.get('/survey', function (request, response) {
+		response.sendFile(path.join(__dirname + '/../public/survey.html'));
+	});
+
+	// If no matching route is found default to home
+	app.use(function (request, response) {
+		response.sendFile(path.join(__dirname + '/../public/home.html'));
+	});
+};
